31d8a497572ade8ae400cc2fb8855aa32e625ce3
Stefan Boos <stefan.boos@gmx.de>
2017-12-05 06:17:00 +0100

    Starting to implement the View and ViewModel for RenameProjectView

--- src/main/java/com/github/wonderbird/RenameProject/ViewModels/RenameProjectViewModel.java
+++ src/main/java/com/github/wonderbird/RenameProject/ViewModels/RenameProjectViewModel.java
@@ -1,6 +1,35 @@
 package com.github.wonderbird.RenameProject.ViewModels;
 
 import de.saxsys.mvvmfx.ViewModel;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 public class RenameProjectViewModel implements ViewModel {
+    public StringProperty from = new SimpleStringProperty("ProjectToBeRenamed");
+
+    public StringProperty to = new SimpleStringProperty("TargetName");
+
+    public StringProperty fromProperty() {
+        return from;
+    }
+
+    public String getFrom() {
+        return from.get();
+    }
+
+    public void setFrom(String aFrom) {
+        from.set(aFrom);
+    }
+
+    public StringProperty toProperty() {
+        return to;
+    }
+
+    public String getTo() {
+        return to.get();
+    }
+
+    public void setTo(String aTo) {
+        to.set(aTo);
+    }
 }
--- src/main/java/com/github/wonderbird/RenameProject/Views/RenameProjectView.java
+++ src/main/java/com/github/wonderbird/RenameProject/Views/RenameProjectView.java
@@ -2,13 +2,27 @@ package com.github.wonderbird.RenameProject.Views;
 
 import com.github.wonderbird.RenameProject.ViewModels.RenameProjectViewModel;
 import de.saxsys.mvvmfx.FxmlView;
+import de.saxsys.mvvmfx.InjectViewModel;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class RenameProjectView implements FxmlView<RenameProjectViewModel>, Initializable {
+    @FXML
+    private TextField fromTextField;
+
+    @FXML
+    private TextField toTextField;
+
+    @InjectViewModel
+    private RenameProjectViewModel viewModel;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        fromTextField.textProperty().bind(viewModel.fromProperty());
+        toTextField.textProperty().bind(viewModel.toProperty());
     }
 }
