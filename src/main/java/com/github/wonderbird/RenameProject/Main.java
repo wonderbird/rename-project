389a4dfe05f309fa8a515b343c9d75b460530ba4
Stefan Boos <stefan.boos@gmx.de>
2017-12-11 05:33:20 +0100

    Finishing GUI implementation, improving overall structure

--- src/main/java/com/github/wonderbird/RenameProject/Main.java
+++ src/main/java/com/github/wonderbird/RenameProject/Main.java
@@ -1,104 +1,29 @@
 package com.github.wonderbird.RenameProject;
 
-import com.github.wonderbird.RenameProject.FileSystemAccess.Implementation.FileContentFinderImpl;
-import com.github.wonderbird.RenameProject.FileSystemAccess.Implementation.FileNamePatternFinderImpl;
-import com.github.wonderbird.RenameProject.FileSystemAccess.Implementation.FileSystemMethodsImpl;
-import com.github.wonderbird.RenameProject.FileSystemAccess.Interfaces.FilePathFinder;
-import com.github.wonderbird.RenameProject.FileSystemAccess.Interfaces.FileSystemMethods;
-import com.github.wonderbird.RenameProject.ViewModels.RenameProjectViewModel;
-import com.github.wonderbird.RenameProject.Views.RenameProjectView;
-import de.saxsys.mvvmfx.FluentViewLoader;
-import de.saxsys.mvvmfx.MvvmFX;
-import de.saxsys.mvvmfx.ViewTuple;
-import de.saxsys.mvvmfx.utils.notifications.NotificationCenter;
-import javafx.application.Application;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import com.github.wonderbird.RenameProject.Logic.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
 
-import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
-
-public class Main extends Application {
-    private static Configuration config = Configuration.getConfiguration();
+public class Main {
 
     private static ArgumentParser argumentParser = new ArgumentParserImpl();
-
-    private static FilePathFinder fileContentFinder = new FileContentFinderImpl();
-
-    private static FilePathFinder fileNamePatternFinder = new FileNamePatternFinderImpl();
-
-    private static FileSystemMethods fileSystemMethods = new FileSystemMethodsImpl();
-
+    private static RenameProjectManager renameProjectManager = new RenameProjectManagerImpl();
     private static Logger logger = LoggerFactory.getLogger(Main.class);
+    private static UiManager uiManager = new UiManagerImpl();
 
     public static void main(String[] args) {
         try {
             argumentParser.parse(args);
 
-            renameProject();
+            renameProjectManager.renameProject();
         } catch (WrongUsageException aException) {
             System.out.println(aException.getLocalizedMessage());
 
-            launch(args);
-        }
-    }
-
-    private static void renameProject() {
-        logger.info("Renaming from '{}' to '{}' ...", config.getFrom(), config.getTo());
-        try {
-
-            renameFilesAndDirectories();
-
-            replaceFileContents();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        ViewTuple<RenameProjectView, RenameProjectViewModel> viewTuple = FluentViewLoader.fxmlView(RenameProjectView.class).load();
-        Parent root = viewTuple.getView();
-        primaryStage.setTitle("Rename Project");
-        primaryStage.setScene(new Scene(root));
-
-        NotificationCenter notificationCenter = MvvmFX.getNotificationCenter();
-        notificationCenter.subscribe(Notification.QUIT.toString(), (key, payload) -> primaryStage.close());
-        notificationCenter.subscribe(Notification.RENAME.toString(), (key, payload) -> {
-            renameProject();
-            primaryStage.close();
-        });
-
-        primaryStage.show();
-    }
-
-    private static void renameFilesAndDirectories() throws IOException {
-        String filePattern = "*" + config.getFrom() + "*";
-        List<Path> affectedPaths = fileNamePatternFinder.find(filePattern);
-
-        for (Path sourcePath : affectedPaths) {
-            Path targetPath = Paths.get(sourcePath.toString().replace(config.getFrom(), config.getTo()));
-
-            logger.info("{} -> {}", sourcePath.toString(), targetPath.toString());
-
-            fileSystemMethods.move(sourcePath, targetPath, REPLACE_EXISTING);
-        }
-    }
-
-    private static void replaceFileContents() throws IOException {
-        List<Path> affectedPaths = fileContentFinder.find(config.getFrom());
-
-        for (Path path : affectedPaths) {
-            logger.info("Replace contents: {}", path.toString());
-
-            fileSystemMethods.replaceInFile(path, config.getFrom(), config.getTo());
+            uiManager.runUi(args);
+        } catch (IOException aException) {
+            logger.error(aException.toString());
         }
     }
 
@@ -106,19 +31,11 @@ public class Main extends Application {
         argumentParser = aArgumentParser;
     }
 
-    static void setFileNamePatternFinder(final FilePathFinder aFinder) {
-        fileNamePatternFinder = aFinder;
+    static void setRenameProjectManager(RenameProjectManager aRenameProjectManager) {
+        renameProjectManager = aRenameProjectManager;
     }
 
-    static void setFileContentFinder(FilePathFinder aFinder) {
-        fileContentFinder = aFinder;
-    }
-
-    static void setFileSystemMethods(FileSystemMethods aFileSystemMethods) {
-        fileSystemMethods = aFileSystemMethods;
-    }
-
-    static void setLogger(Logger aLogger) {
-        logger = aLogger;
+    static void setUiManager(UiManager aUiManager) {
+        uiManager = aUiManager;
     }
 }
