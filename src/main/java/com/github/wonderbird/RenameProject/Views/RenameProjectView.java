79488bf04bd81d5a4ae7c6c0397d7edee327cce4
Stefan Boos <stefan.boos@gmx.de>
2017-12-13 06:12:47 +0100

    Allowing to configure the start directory in the UI

--- src/main/java/com/github/wonderbird/RenameProject/Views/RenameProjectView.java
+++ src/main/java/com/github/wonderbird/RenameProject/Views/RenameProjectView.java
@@ -1,14 +1,15 @@
 package com.github.wonderbird.RenameProject.Views;
 
+import com.github.wonderbird.RenameProject.Models.Notification;
 import com.github.wonderbird.RenameProject.ViewModels.RenameProjectViewModel;
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.InjectViewModel;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -22,9 +23,6 @@ public class RenameProjectView implements FxmlView<RenameProjectViewModel>, Init
     @FXML
     private TextField startDirTextField;
 
-    @FXML
-    private Button cancelButton;
-
     @InjectViewModel
     private RenameProjectViewModel viewModel;
 
@@ -33,6 +31,17 @@ public class RenameProjectView implements FxmlView<RenameProjectViewModel>, Init
         fromTextField.textProperty().bind(viewModel.fromProperty());
         toTextField.textProperty().bind(viewModel.toProperty());
         startDirTextField.textProperty().bind(viewModel.startDirProperty());
+
+        viewModel.subscribe(Notification.BROWSESTARTDIR.toString(), (key, payload) -> browseStartDir((String)payload[0]));
+    }
+
+    private void browseStartDir(String initialDir) {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setInitialDirectory(new File(initialDir));
+        File file = directoryChooser.showDialog(fromTextField.getScene().getWindow());
+        if (file != null) {
+            viewModel.setStartDir(file.getPath());
+        }
     }
 
     @FXML
