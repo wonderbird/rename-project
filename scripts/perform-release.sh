2d5b321d60c0cf26740d26505f0036040c40bc1a
Stefan Boos <stefan.boos@gmx.de>
2018-01-22 06:15:23 +0100

    (Start) Implementing automatic releases

--- scripts/perform-release.sh
+++ scripts/perform-release.sh
@@ -8,11 +8,14 @@ echo =====
 
 # Use the Maven Release Plugin to create a release non-interactively
 # See also: http://maven.apache.org/maven-release/maven-release-plugin/usage.html
-mvn -DdryRun=true -Dusername="Travis CI <stefan.boos@gmx.de>" release:prepare
+mvn -DdryRun=true -DscmCommentPrefix="[skip ci] " release:prepare
+
+# TODO: Publish the .dmg file to the GitHub release page
+# Read: https://developer.github.com/v3/repos/releases/#create-a-release
 
 # In order to test the release, run the following command
 #
-#mvn -DdryRund=true release:prepare
+#mvn -DdryRun=true release:prepare
 #
 # Then check the outputs and clean up
 #
