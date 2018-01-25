65aa6f0f0c46d38f08a739ab7f4a14d96397a12d
Stefan Boos <stefan.boos@gmx.de>
2018-01-25 05:31:51 +0100

    (In progress) Implementing automatic releases

--- scripts/perform-release.sh
+++ scripts/perform-release.sh
@@ -3,12 +3,44 @@
 # Create a release on GitHub
 
 echo =====
-echo Creating a release on GitHub
+echo Parsing Project Version
+echo =====
+
+# Transform the current version to the release tag name by truncating the
+# "-SNAPSHOT" suffix and prepending the artifact id
+CURRENT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)')
+RELEASE_VERSION=$(echo $CURRENT_VERSION | sed s/-SNAPSHOT//)
+
+ARTIFACT_ID=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.artifactId | grep -Ev '(^\[|Download\w+:)')
+RELEASE_TAG=$ARTIFACT_ID-$RELEASE_VERSION
+
+echo Release Tag in GitHub: $RELEASE_TAG
+
+echo
+echo =====
+echo Preparing Release
 echo =====
 
 # Use the Maven Release Plugin to create a release non-interactively
 # See also: http://maven.apache.org/maven-release/maven-release-plugin/usage.html
-mvn -DdryRun=true -DscmCommentPrefix="[skip ci] " release:prepare
+mvn -DdryRun=true -DscmCommentPrefix="[skip ci] " release:prepare -Dresume=false
+RELEASE_PREPARE_SUCCESS=$?
+if [ $RELEASE_PREPARE_SUCCESS -ne 0 ]; then
+    mvn -DdryRun=true -DscmCommentPrefix="[skip ci] " release:clean
+
+    echo
+    echo "ERROR: Failed to prepare the release '$RELEASE_TAG.'"
+    echo "       The release has been cleaned, please fix the error(s) and try again."
+
+    exit 1
+fi
+
+echo
+echo =====
+echo Performing Release
+echo =====
+
+mvn -DdryRun=true -DscmCommentPrefix="[skip ci] " release:perform
 
 # TODO: Publish the .dmg file to the GitHub release page
 # GraphQL 4 API: https://developer.github.com/v4/
