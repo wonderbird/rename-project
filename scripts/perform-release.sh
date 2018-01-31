9ad2b91dc31f07f811b4fd30778c367960d29d00
Stefan Boos <stefan.boos@gmx.de>
2018-01-31 08:46:43 +0100

    Fixed release upload

--- scripts/perform-release.sh
+++ scripts/perform-release.sh
@@ -93,7 +93,7 @@ fi
 echo "Created release with id $RELEASE_ID"
 
 echo "Uploading release artifact to GitHub using $UPLOAD_URL ..."
-POST_UPLOAD_RESPONSE=$(curl --verbose --data-binary '@./target/$RELEASE_TAG.dmg' --header "content-type: application/octet-stream" --header "authorization: bearer $GITHUB_ACCESS_TOKEN" $UPLOAD_URL)
+POST_UPLOAD_RESPONSE=$(curl -s --data-binary "@./target/${RELEASE_TAG}.dmg" --header "content-type: application/octet-stream" --header "authorization: bearer $GITHUB_ACCESS_TOKEN" $UPLOAD_URL)
 POST_UPLOAD_STATE=$(echo $POST_UPLOAD_RESPONSE | jq '.state' | sed 's/"//g')
 POST_UPLOAD_SUCCESS=$?
 
