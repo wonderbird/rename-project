fb06b63fc0437874d91ac4c9ee2fc751f521ff16
Stefan Boos <stefan.boos@gmx.de>
2019-05-21 08:56:28 +0200

    chore: use the github-release-plugin
    see also: https://synyx.de/blog/2018-01-24-travisci-github-releases/
    and: https://github.com/jutzig/github-release-plugin

---
+++ .travis/perform-release.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# Create a release on GitHub
+# adopted from https://synyx.de/blog/2018-01-24-travisci-github-releases/
+# and from https://github.com/jutzig/github-release-plugin
+
+DRY_RUN=false
+SCRIPTNAME=$(basename "$0")
+
+echo =====
+echo Releasing to GitHub
+echo =====
+
+echo mvn deploy --settings .travis/settings.xml -Drevision=${TRAVIS_BUILD_NUMBER} -DskipTests=true --batch-mode --update-snapshots -Prelease
