02c6aa41ca136b70157f0cc198031d631e9ce122
Stefan Boos <stefan.boos@gmx.de>
2019-05-23 10:04:01 +0200

    chore: cosmetics in release script; deploy a release only from master

--- .travis/perform-release.sh
+++ .travis/perform-release.sh
@@ -5,15 +5,24 @@
 # and from https://github.com/jutzig/github-release-plugin
 
 SETTINGS=.travis/settings.xml
+MVN="mvn --settings $SETTINGS -Drevision=${TRAVIS_BUILD_NUMBER} -DskipTests=true --batch-mode"
 
 echo =====
 echo Creating the release artifact
 echo =====
 
-mvn package --settings "$SETTINGS" -Drevision=${TRAVIS_BUILD_NUMBER} -DskipTests=true --batch-mode
+$MVN package
+
+echo =====
+echo Renaming the release artifact
+echo =====
+
+PROJECT_VERSION=$($MVN org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -E '^[^\[]')
+PROJECT=javafx-ui
+echo mv $PROJECT/target/$PROJECT-$PROJECT_VERSION.dmg $PROJECT/target/rename-project-$PROJECT_VERSION.dmg
 
 echo =====
 echo Releasing to GitHub
 echo =====
 
-mvn github-release:release -Dgithub.draft=true --projects javafx-ui --settings "$SETTINGS" -Drevision=${TRAVIS_BUILD_NUMBER} -DskipTests=true --batch-mode
+$MVN github-release:release --projects javafx-ui
